6c8d2a9cb24017ffd5d5f08d1b0a4f389580e33c
sourcetreewithgit <sourcetreewithgit@gmail.com>
2018-03-10 17:53:23 +0530

    rtyuio

--- GST_Impl/Controllers/ClientController.cs
+++ GST_Impl/Controllers/ClientController.cs
@@ -7,7 +7,7 @@ using GST_Impl.Models;
 
 namespace GST_Impl.Controllers
 {
-    //Rohan Satote 1111133333333555555 qqq 1234
+    //Rohan Satote 1111133333333555555 qqq 1234 new
     public class ClientController : Controller
     {
         private GSTBookEntities db = new GSTBookEntities();
